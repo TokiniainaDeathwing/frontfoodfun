247a3eca97c88f62e45349c13733d1dbb601debc
tokiniaina <tokiniainaherve.andrianarison@gmail.com>
2019-05-01 21:27:22 +0300

    Integration template part 2:menu,contact,reservation;

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,10 +8,28 @@ use Twig\Environment;
 
 class IndexController
 {
+  // home page
   public function index(Environment $twig)
   {
-    $content = $twig->render('index.html.twig');
+    $content = $twig->render('accueil.html.twig');
 
     return new Response($content);
   }
+
+  // liste des plats
+  public function menu_page(Environment $twig)
+  {
+    $content = $twig->render('menu.html.twig');
+
+    return new Response($content);
+  }
+
+  // Contact
+  public function contact_page(Environment $twig)
+  {
+    $content = $twig->render('contact.html.twig');
+
+    return new Response($content);
+  }
+
 }
