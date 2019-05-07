e194698bf88b765a512ca73e5aaa8647ce43a2b6
tokiniaina <tokiniainaherve.andrianarison@gmail.com>
2019-05-07 23:37:09 +0300

    update plat repository

--- src/Repository/PlatRepository.php
+++ src/Repository/PlatRepository.php
@@ -64,7 +64,7 @@ class PlatRepository extends ServiceEntityRepository
          FROM plat p JOIN
           images i on i.idplat=p.id
           WHERE i.type=0
-          ORDER BY RAND() LIMIT 6;
+          ORDER BY RAND() LIMIT 3;
           
         ';
         $stmt = $conn->prepare($sql);
@@ -97,7 +97,7 @@ class PlatRepository extends ServiceEntityRepository
          ca on ca.idplat=p.id 
          JOIN categorie c ON c.id=ca.idcategorie JOIN
           images i on i.idplat=p.id
-          WHERE c.nom = :cat AND i.type=:type;
+          WHERE c.nom = :cat AND i.type=:type group by p.id order by p.id limit;
         ';
         $stmt = $conn->prepare($sql);
         $stmt->execute(['cat' => $categorie,'type'=>$type]);
@@ -124,7 +124,7 @@ class PlatRepository extends ServiceEntityRepository
         if($categorie!=""){
             $sql=$sql." and c.nom='".$categorie."'";
         }
-        $sql=$sql." order by p.id limit ".$offset.",".$limit;
+        $sql=$sql."group by p.id order by p.id limit ".$offset.",".$limit;
         $sql=$sql.";";
         $stmt = $conn->prepare($sql);
         $stmt->execute(['type'=>$typeimage]);
@@ -137,7 +137,8 @@ class PlatRepository extends ServiceEntityRepository
         $conn = $this->getEntityManager()->getConnection();
 
         $sql = '
-        SELECT count(*) as nombre
+        SELECT count(*) as nombre FROM(
+          SELECT p.*,c.nom as categorie,i.url,i.description as descriptionimage,i.type
          FROM plat p 
          JOIN categorieplat 
          ca on ca.idplat=p.id 
@@ -150,6 +151,7 @@ class PlatRepository extends ServiceEntityRepository
         if($categorie!=""){
             $sql=$sql." and c.nom='".$categorie."'";
         }
+        $sql=$sql." group by p.id order by p.id ) as t1";
         $sql=$sql.";";
         $stmt = $conn->prepare($sql);
         $stmt->execute(['type'=>$typeimage]);
